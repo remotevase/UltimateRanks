9a043880ac1d2d625bb535d1bf4c08e4ad71bc00
remotevase <web.articlerequests@gmail.com>
2016-04-14 20:06:47 -0400

    @taki21 so far good?

--- src/ColorArmour/rewrite.php
+++ src/ColorArmour/rewrite.php
@@ -31,4 +31,35 @@ class ColorArmour extends PluginBase implements Listener {
       $colors = new Config($this->getDataFolder() . "/rank.yml", Config::YAML);
         $colors->save();
     $this->getLogger()->info(C::YELLOW . "ColorArmour Configs Saved!");
+    $this->YTHead = $youtuber->get("Helmet")
+    $this->YTChest = $youtuber->get("Chestplate")
+    $this->YTLegs = $youtuber->get("Pants")
+    $this->YTFeet = $youtuber->get("Boots")
+    
+    $this->RUHead = $regularuser->get("Helmet")
+    $this->RUChest = $regularuser->get("Chestplate")
+    $this->RULegs = $regularuser->get("Pants")
+    $this->RUFeet = $regularuser->get("Boots")
+    
+    $this->OTHead = $others->get("Helmet")
+    $this->OTChest = $others->get("Chestplate")
+    $this->OTLegs = $others->get("Pants")
+    $this->OTFeet = $others->get("Boots")
+    
+    $this->VPHead = $vip->get("Helmet")
+    $this->VPChest = $vip->get("Chestplate")
+    $this->VPLegs = $vip->get("Pants")
+    $this->VPFeet = $vip->get("Boots")
+    
+    $this->VPLHead = $viplus->get("Helmet")
+    $this->VPLChest = $viplus->get("Chestplate")
+    $this->VPLLegs = $viplus->get("Pants")
+    $this->VPLFeet = $viplus->get("Boots")
+    
+    $this->CLYT = #$vip->get("Helmet")
+    $this->CLRU = #$vip->get("Chestplate")
+    $this->CLOT = #$vip->get("Pants")
+    $this->CLVP = #$vip->get("Boots")
+    $this->CLVPL = #$vip->get("Boots")
+    
   }
