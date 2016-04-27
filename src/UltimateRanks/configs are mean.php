30287a66cc1f74b108a13a73ae233ed4cd3792e6
remotevase <web.articlerequests@gmail.com>
2016-04-27 17:00:36 -0400

    Delete configs are mean.php

--- src/UltimateRanks/configs are mean.php
+++
@@ -1,7 +0,0 @@
-if($colors->get("YoutuberColor")==null){
-                $colors->set("YoutuberColor", "6618880");
-                $colors->set("RegularUserColor", "9541888");
-                $colors->set("OthersColor", "16711680");
-                $colors->set("VIPColor", "0");
-                $colors->set("VIP+Color", "16773888");
-                }
